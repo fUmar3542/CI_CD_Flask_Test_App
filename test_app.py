3f03c0578e698d23d09acbacf78c5bcfdacd31a1
Muhammad Umar Farooq <fumar3542@gmail.com>
2024-09-01 16:27:34 +0500

    Updated

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ from app import app
 def test_hello_world():
     client = app.test_client()
     response = client.get('/')
-    assert response.data == b'Hello, World!'
+    assert response.data == b'Hello, world!'
